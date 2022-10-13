d9f4ed05d3bac57bf96350d4a680ac02692e1068
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2022-10-13 13:34:40 +0800

    Added normalize.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // `pages/_app.js`
 import '../styles/global.css';
+import 'normalize.css/normalize.css';
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />;
